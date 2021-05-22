b390cc4e577f4315c140dfe61af41149daca22b1
Danielid Ramirez <danielid132@gmail.com>
2021-05-22 17:05:47 -0400

    creating query model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_232353) do
+ActiveRecord::Schema.define(version: 2021_05_22_205402) do
+
+  create_table "queries", force: :cascade do |t|
+    t.string "client"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ufs", force: :cascade do |t|
     t.float "value"
